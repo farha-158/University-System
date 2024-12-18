511e87dbfe2a68846dc18fd34833e1f93ce66e67
MHameer000 <168327176+MHameer000@users.noreply.github.com>
2024-12-19 00:51:50 +0200

    Update AdminController.cs

--- Project/University-System/Controllers/AdminController.cs
+++ Project/University-System/Controllers/AdminController.cs
@@ -1,1 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using SysUniPro.Factory;
+using SysUniPro.Models;
+using SysUniPro.Repostory;
 
+namespace SysUniPro.Controllers
+{
+    public class AdminController : Controller
+    {
+        //private readonly IAdminRepostory _adminRepostory;
+        private readonly IRepository<Admin> _adminRepository;
+
+        public AdminController(RepositoryFactory factory)
+        {
+            _adminRepository = factory.CreateRepository<Admin>();
+        }
+
+        //public AdminController(IAdminRepostory adminRepostory) {
+        //    _adminRepostory = adminRepostory;
+        //}
+        public ActionResult Index()
+        {
+            var admins = _adminRepository.GetAll();
+            return View( admins);
+        }
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAdmin(Admin admin)
+        {
+            _adminRepository.Add(admin);
+            var admins = _adminRepository.GetAll();
+            return View("Index", admins);
+        }
+
+        public ViewResult Delete(int id)
+        {
+            _adminRepository.Delete(id);
+            var admins = _adminRepository.GetAll();
+            return View("Index", admins);
+        }
+    }
+}
